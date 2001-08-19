74dc3c1ed7b8df3835a2998884e37e42588360ee
R.J. Clay <jame@users.sourceforge.net>
2001-08-19 19:47:32 +0000

    Initial revision
    git-svn-id: https://ftnpl.svn.sourceforge.net/svnroot/ftnpl/trunk/modules/FTN/Packet@9 987ebf5b-b7b1-44df-af0d-bd292689ea74

---
+++ Packet.pm
@@ -0,0 +1,51 @@
+package FTN::Packet;
+
+use strict;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+
+require Exporter;
+require AutoLoader;
+
+@ISA = qw(Exporter AutoLoader);
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+@EXPORT = qw(
+	
+);
+$VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+# Autoload methods go after =cut, and are processed by the autosplit program.
+
+1;
+__END__
+# Below is the documentation for this module. 
+
+=head1 NAME
+
+FTN::Packet - Perl extension for Fidonet Technology Networks Packet files
+
+=head1 SYNOPSIS
+
+  use FTN:Packet;
+  To be added...
+
+=head1 DESCRIPTION
+
+Stub documentation for FTN::Packet was created initially. I will add to this
+as I have something working.
+
+To be added.
+
+=head1 AUTHOR
+
+Robert James Clay (R.J. Clay), jame@users.sf.net
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
