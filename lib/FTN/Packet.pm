76d39cf2a7048643ffa8b4f261aa0c6f1f70f4ad
R.J. Clay <jame@users.sourceforge.net>
2011-07-11 17:09:42 -0400

    Set scope for ISA, EXPORT, and EXPORT_OK in Packet.pm

--- lib/FTN/Packet.pm
+++ lib/FTN/Packet.pm
@@ -31,13 +31,13 @@ write_ftn_packet().
 
 =cut
 
-@ISA = qw(Exporter AutoLoader);
+our @ISA = qw(Exporter AutoLoader);
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
-@EXPORT = qw(
+our @EXPORT = qw(
 );
-@EXPORT_OK = qw( &read_ftn_packet &write_ftn_packet
+our @EXPORT_OK = qw( &read_ftn_packet &write_ftn_packet
 );
 
 =head1 FUNCTIONS
