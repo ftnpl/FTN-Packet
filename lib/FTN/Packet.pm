f2fb82ddf00afbeacd80067f1c1d1e444df7aadb
RJ Clay <jame@rocasa.us>
2012-09-03 07:16:07 -0400

    Bump version to v0.16

--- lib/FTN/Packet.pm
+++ lib/FTN/Packet.pm
@@ -9,11 +9,11 @@ FTN::Packet - Reading or writing Fidonet Technology Networks (FTN) packets.
 
 =head1 VERSION
 
-VERSION 0.15
+VERSION 0.16
 
 =cut
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 =head1 DESCRIPTION
 
